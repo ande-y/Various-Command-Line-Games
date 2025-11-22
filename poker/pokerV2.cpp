0ca2902151d24817eb3e20410d3c48272ddf2346
ande-y <andy.cheng0814@gmail.com>
2025-11-22 02:17:49 -0500

    pokerV2 major issue fixed

--- poker/pokerV2.cpp
+++ poker/pokerV2.cpp
@@ -16,18 +16,23 @@ string RANKS[] = {"2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "
 #define RAISE 3
 #define ALLIN 4
 
-typedef string communityCardGraphics[5][7];
-communityCardGraphics
-    show = {{"┏", "━", "━", "━", "━", "━", "┓"},
-            {"┃", " ", " ", " ", " ", " ", "┃"},
-            {"┃", " ", " ", " ", " ", " ", "┃"},
-            {"┃", " ", " ", " ", " ", " ", "┃"},
-            {"┗", "━", "━", "━", "━", "━", "┛"}},
-    hide = {{"┌", "┄", "┄", "┄", "┄", "┄", "┐"},
-            {"┊", " ", " ", " ", " ", " ", "┊"},
-            {"┊", " ", " ", " ", " ", " ", "┊"},
-            {"┊", " ", " ", " ", " ", " ", "┊"},
-            {"└", "┄", "┄", "┄", "┄", "┄", "┘"}};
+typedef string cardGraphics[5][7];
+cardGraphics
+    hideCard = {{"┏", "━", "━", "━", "━", "━", "┓"},
+                {"┃", "░", "░", "░", "░", "░", "┃"},
+                {"┃", "░", "░", "░", "░", "░", "┃"},
+                {"┃", "░", "░", "░", "░", "░", "┃"},
+                {"┗", "━", "━", "━", "━", "━", "┛"}},
+    showCard = {{"┏", "━", "━", "━", "━", "━", "┓"},
+                {"┃", " ", " ", " ", " ", " ", "┃"},
+                {"┃", " ", " ", " ", " ", " ", "┃"},
+                {"┃", " ", " ", " ", " ", " ", "┃"},
+                {"┗", "━", "━", "━", "━", "━", "┛"}},
+    foldCard = {{"┌", "┄", "┄", "┄", "┄", "┄", "┐"},
+                {"┊", " ", " ", " ", " ", " ", "┊"},
+                {"┊", " ", " ", " ", " ", " ", "┊"},
+                {"┊", " ", " ", " ", " ", " ", "┊"},
+                {"└", "┄", "┄", "┄", "┄", "┄", "┘"}};
 
 struct Card {
     int suit;
@@ -35,15 +40,10 @@ struct Card {
     bool show;
 };
 
-void thrErr(string s){
-    cout << "<!> " << s << endl;
-    exit(1);
-}
-
 class Table {
     private:
-    static vector<Card> deck;
-    static vector<Card> communityCards;
+    vector<Card> deck;
+    vector<Card> communityCards;
 
     public:
     Table(){
@@ -69,9 +69,8 @@ class Table {
         return c;
     }
 
-    void drawCommunityCard(){
-        communityCards.push_back(getCard());
-        if (communityCards.size() > 5) thrErr("Table:drawCommunityCard: attempted to insert more than 6 community cards");
+    void drawCommunityCard(Card c){
+        communityCards.push_back(c);
     }
 };
 
@@ -81,13 +80,15 @@ class Player {
     string name;
     int chips;
     int bet;
-    Card hand[0];
+    Card hand[2];
 
     public:
     Player(bool a, string b, int c):
         playable(a), name(b), chips(c){}
     
-    void recieveCard(Card c, Card d){
+    string getName(){ return name; }
+
+    void recieveCards(Card c, Card d){
         hand[0] = c;
         hand[1] = d;
     }
@@ -96,9 +97,9 @@ class Player {
         int betSize;
         if (playable){
             do {
-                cout << "► Make an blind bet: ";
+                cout << "□ The current bet is " << currentBet << "\n► Make an blind bet: ";
                 cin >> betSize;
-            } while (betSize <= currentBet || betSize >= chips);
+            } while (betSize < currentBet || betSize >= chips);
         }
         else {
             betSize = currentBet + 1;
@@ -115,14 +116,13 @@ class Player {
             do {
                 cout << "► Make decision [0:Fold][1:Call][2:Check][3:Raise][4:AllIn]: ";
                 cin >> decision;
-            } while (decision < 0 || decision > 4);
-
+            } while (decision < 0 && decision > 4);
             if (decision == RAISE){
                 int raiseBy;
                 do {
                     cout << "► Raise by: ";
                     cin >> raiseBy;
-                } while (raiseBy < 0 || raiseBy > chips);
+                } while (raiseBy < 0 && raiseBy > chips);
 
                 chips -= raiseBy;
                 currentBet += raiseBy;
@@ -140,11 +140,31 @@ class Player {
     }
 };
 
-void bettingCycle(vector<Player>& players, int turn, int& betSize){
+void printTable(vector<Player> players, Table table){
+    vector<Card> communityCards = table.getCommunityCards();
+    for (int r = 0; r < 5; r++){
+        for (int i = 0; i < 5; i++){
+            for (int c = 0; c < 7; c++){
+                if (i + 1 > communityCards.size()){
+                    cout << foldCard[r][c];
+                }
+                else {
+                    if (r == 1 && c == 1 || r == 3 && c == 5) cout << RANKS[communityCards[i].rank];
+                    else if (communityCards[i].rank == 8 && (r == 1 && c == 2 || r == 3 && c == 3)); 
+                    else if (r == 2 && c == 3) cout << SUITS[communityCards[i].suit];
+                    else cout << showCard[r][c];
+                }
+            }
+        }
+        cout << endl;
+    }
+}
+
+void bettingCycle(vector<Player>& players, Table table, int turn, int& betSize){
     int size = players.size();
     int action[size];
-    for (int a: action) a = NONE;
-    bool settled = 0;
+    for (int& a: action) a = NONE;
+    int settled = 0;
 
     while (settled < size){
         for (int i = 0; i < size; i++){
@@ -165,39 +185,43 @@ void bettingCycle(vector<Player>& players, int turn, int& betSize){
             else {
                 settled++;
             }
+
+            printTable(players, table);
         }
-        for (int a: action){
+        for (int& a: action){
             if (a == CHECK) a = NONE;
         }
     }
 }
 
-void playGame(vector<Player>& players, Table table, int dealer, int betSize){
+void playGame(vector<Player>& players, Table table, int dealer, int initialBet){
     int size = players.size();
-    int turn = dealer;
+    int currentBet = initialBet;
 
     // initiate blind bets
-    betSize = players[turn = (turn + 1) % size].blindBet(betSize);
-    betSize = players[turn = (turn + 1) % size].blindBet(betSize);
+    int turn = (dealer + 1) % size;
+    players[turn].blindBet(currentBet);
+    turn = (dealer + 1) % size;
+    players[turn].blindBet(currentBet);
 
-    // give all players 2 cards
-    for (Player p: players) p.recieveCard(table.getCard(), table.getCard());
-    bettingCycle(players, turn, betSize);
+    for (Player& p: players) p.recieveCards(table.getCard(), table.getCard());
+    bettingCycle(players, table, turn, currentBet);
 
     // the flop
-    table.drawCommunityCard();
-    table.drawCommunityCard();
-    table.drawCommunityCard();
-    bettingCycle(players, turn, betSize);
+    table.drawCommunityCard(table.getCard());
+    table.drawCommunityCard(table.getCard());
+    table.drawCommunityCard(table.getCard());
+    bettingCycle(players, table, turn, currentBet);
 
-    table.drawCommunityCard();
-    bettingCycle(players, turn, betSize);
+    table.drawCommunityCard(table.getCard());
+    bettingCycle(players, table, turn, currentBet);
 
     // the river
-    table.drawCommunityCard();
-    bettingCycle(players, turn, betSize);
+    table.drawCommunityCard(table.getCard());
+    bettingCycle(players, table, turn, currentBet);
 
     // calculate
+    cout << "completed\n";
 }
 
 int main(){
@@ -208,17 +232,14 @@ int main(){
     Player p2(false, "opp2", 100);
     Player p3(false, "opp3", 100);
 
+
     vector<Player> players = {p0, p1, p2, p3};
     Table table;
 
-    int dealer = rand() % 4;
-    int initBetSize = 4;
+    int dealer = rand() % players.size(); 
+    int initialBet = 4;
 
-    while (true){
-        playGame(players, table, dealer, initBetSize);
-        dealer = (dealer + 1) % players.size();
-        initBetSize *= 1.5;
-    }
+    playGame(players, table, dealer, initialBet);
 
     return 0;
 }
